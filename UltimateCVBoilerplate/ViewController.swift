30ad0dfceeccbdd90c86965a8bef99a13a734592
gladiusKatana <garthsnyder@protonmail.com>
2019-04-03 14:33:55 -0400

    Initial Commit

---
+++ UltimateCVBoilerplate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UltimateCVBoilerplate
+//
+//  Created by Garth Snyder on 2019-04-03.
+//  Copyright © 2019 Garth Snyder. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
